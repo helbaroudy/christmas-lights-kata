2780c721e5138d93f93892628d8443d6d3a07f7d
Hussein Kasem <Hussein.Kasem@thetrainline.com>
2023-12-08 21:26:56 +0100

    Refactor

--- src/main/kotlin/ChristmasLights.kt
+++ src/main/kotlin/ChristmasLights.kt
@@ -6,28 +6,25 @@ class ChristmasLights {
     val lights = MutableList(1000) { MutableList(1000) { 0 } }
 
     fun turnOn(fromX: Int, fromY: Int, toX: Int, toY: Int) {
-        for (i in fromX until toX) {
-            for (j in fromY until toY) {
-                lights[i][j] = 1
-            }
-        }
+        setValue(fromX, toX, fromY, toY) { _, _ -> 1 }
     }
 
     fun turnOff(fromX: Int, fromY: Int, toX: Int, toY: Int) {
+        setValue(fromX, toX, fromY, toY) { _, _ -> 0 }
+    }
+
+    fun switch(fromX: Int, fromY: Int, toX: Int, toY: Int) {
+        setValue(fromX, toX, fromY, toY) { i, j -> lights[i][j].switch() }
+    }
+
+    private fun setValue(fromX: Int, toX: Int, fromY: Int, toY: Int, value: (Int, Int) -> Int) {
         for (i in fromX until toX) {
             for (j in fromY until toY) {
-                lights[i][j] = 0
+                lights[i][j] = value(i, j)
             }
         }
     }
 
-    fun switch(fromX: Int, fromY: Int, toX: Int, toY: Int) {
-        for (i in fromX until toX) {
-            for (j in fromY until toY) {
-                lights[i][j] = lights[i][j].switch()
-            }
-        }
-    }
 }
 
 private fun Int.switch(): Int =
